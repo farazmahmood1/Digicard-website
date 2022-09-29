bbd9ce92ba69d1ca209fd0b11ffdba6b683e6ea6
TOSHIBA <faraz.m4765@gmail.com>
2022-09-29 21:57:50 +0500

    change css

--- src/Components/Cardinfo.jsx
+++ src/Components/Cardinfo.jsx
@@ -9,17 +9,17 @@ import { hover } from '@testing-library/user-event/dist/hover';
 
 
 
-// const customStyles = {
-//   content: {
-//     top: '50%',
-//     left: '50%',
-//     right: 'auto',
-//     bottom: 'auto',
-//     marginRight: '-50%',
-//     transform: 'translate(-50%, -50%)',
-//     backgroundColor: "rgba(0,0,0,0.2)"
-//   },
-// }
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: "rgba(0,0,0,0.2)"
+  },
+}
 
 const Cardinfo = ({ shouldShow, closeModal, data }) => {
   return (
